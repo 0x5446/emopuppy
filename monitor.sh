a06f643683c362a92b08ad2f56b750f4f48bfe78
Frankee <spirittf@gmail.com>
2024-05-26 10:19:40 +0800

    update remote ip

--- monitor.sh
+++ monitor.sh
@@ -3,6 +3,6 @@
 LOCKFILE="/tmp/emopuppy_monitor.lock"
 (
   	flock -n 200 || exit 1
-	cd /home/tf/workspace/emopuppy && sox -t alsa hw:2,0 -c 1 -r 16000 -t raw - 2>/dev/null |nc -vvN pc 8082 | ./monitor.py
+	cd /home/tf/workspace/emopuppy && sox -t alsa hw:2,0 -c 1 -r 16000 -t raw - 2>/dev/null |nc -vvN 81.70.190.164 8082 | ./monitor.py
 ) 200>$LOCKFILE
 
